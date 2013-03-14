af4d7fbec4a2b67eb3736150916fee8c493809da
Alexey Elymanov <aelymanov@begun.ru>
2013-03-15 03:14:52 +0400

    init

---
+++ example.php
@@ -0,0 +1,86 @@
+<?php
+include ('queryDumper.php');
+include ('../local_db_login.php');
+
+/* uses  mysqli::fetch_all(MYSQLI_NUM) 
+ * array(
+ * 0=>array(field1, field2), 
+ * ... 
+ * n=>array(field1,field2)
+ * )
+ * 
+ * we need mysqli metadata
+ * mysqli->fetch_fields() //  array(db, table, origtable)
+ * there are some hacks for getting meta with mysql_ extension on php.net
+ * but mysqli works great
+ * */
+
+//self joins test
+$exampleQuery="select * from employees.employees e1 left join employees.employees e2 on 1=1 limit 1";
+$exampleQuery="select * from information_schema.columns c1 left join information_schema.columns c2 on 1=1 limit 1";
+
+
+
+$exampleMysqli = new mysqli($host, $user, $password, $database);
+$exampleResult=$exampleMysqli->query($exampleQuery);
+
+//example
+//if no mysqlnd (native driver installed)
+if (!method_exists('mysqli_result', 'fetch_all') ){
+    $exampleData=fetchAll($exampleResult);
+}else{
+    $exampleData=$exampleResult->fetch_all(MYSQLI_NUM);
+}    
+$exampleMeta=$exampleResult->fetch_fields();
+   
+
+
+
+/*
+ * field content removal options
+ * column name => function name in queryDumper.php, namespace QueryDumperHelpers
+ * 
+ * */
+ 
+$forbiddenFields=array(
+'password'=>'replacePassword', //change password -> md5("password")
+'login'=>'replaceLogin', //change login vasya@mail.ru -> vasya@example.com
+'comment'=>'sanitizeComment' //lorem ipsum or 
+);
+
+
+
+
+//get tables dump
+$dump=(\queryDumper\dump($exampleData, $exampleMeta, $forbiddenFields));
+
+
+
+$dropDatabase=true; //default false
+$dropTable=true; //default false
+
+$dbAndTablesCreationDump=\QueryDumperDatabaseAndTables\dump($exampleMysqli,$exampleMeta, $dropDatabase, $dropTable);
+
+$databases=$dbAndTablesCreationDump['databases'];
+$tables=$dbAndTablesCreationDump['tables'];
+
+echo implode("\n", $databases)."\n";
+echo implode("\n", $tables)."\n";
+echo "\n";
+echo implode("\n", $dump);
+echo "\n";
+
+
+//mysqlnd not installed
+function fetchAll($result)
+{
+    $rows = array();
+    while($row = $result->fetch_array(MYSQLI_NUM))
+    {
+        $rows[] = $row;
+    }
+    return $rows;
+}
+
+
+?>
---
+++ queryDumper.php
@@ -0,0 +1,143 @@
+<?php
+namespace QueryDumper;
+
+function dump($result, $metadata, $forbiddenFields, $mysqli_link=false){
+    $insertValues=queryParse($result, $metadata, $forbiddenFields);
+    
+    $dump=createDump($insertValues);
+    return $dump;
+}
+
+function queryParse($result, $metadata, $forbiddenFields, $mysqli_link=false){
+ 
+    $tableAliases=array();
+    foreach ($result as $row=>$values){
+        foreach ($metadata as $fc=>$meta){
+            $db=$meta->db;
+            $table=$meta->orgtable;
+            $alias=$meta->table;
+            if (array_key_exists($meta->orgname,$forbiddenFields)){
+                $helperName='\QueryDumperHelpers\\'.$forbiddenFields[$meta->orgname];
+                $values[$fc]=$helperName($values[$fc]);
+            }
+            if ($mysqli_link){
+                $insertValues[$db][$alias][$table][$row][$fc]='"'.$mysqli->real_escape_string($values[$fc]).'"'; 
+            }else{
+                $insertValues[$db][$alias][$table][$row][$fc]='"'.addslashes($values[$fc]).'"'; 
+            }    
+        }
+      
+    }
+    return $insertValues;
+}
+
+function createDump($insertValues){
+    
+    
+    foreach ($insertValues as $db=>$aliases){
+        foreach ($aliases as $alias=>$tables){
+            foreach ($tables as $table=>$rows){
+                
+                $inserts=array();
+                foreach ($rows as $row){
+                    $inserts[]="(".implode(",", $row).")";  
+                }
+                $query="INSERT IGNORE INTO `{$db}`.`{$table}` VALUES\n".implode(",\n",$inserts).";";
+                
+                $queries[]=$query;
+
+            }
+        }
+    }
+    return $queries;
+}
+
+
+namespace QueryDumperHelpers;
+/*
+ * field conversions functions
+ * 
+ * 
+ */
+function replacePassword($input){
+    return md5("password");
+}
+function replaceLogin($input){
+    return preg_replace("/@.*/", "@example.com", $input);
+} 
+
+function sanitizeComment($input){
+    //$generator = new LoremIpsumGenerator;
+    //return $generator->getContent(100, 'plain');
+    return "test comment\n\n ...ok\n\n";
+}
+
+namespace QueryDumperDatabaseAndTables;
+//uses mysqli
+//todo: use callback functions
+
+
+function dump($mysqli, $metadata, $dropDatabase=false, $dropTable=false){
+    
+    $dump=DatabasesAndTables($mysqli, $metadata);
+    $createDatabases=array();
+    $createTables=array();
+    
+    $dbExists=" IF NOT EXISTS ";
+    $dropDb="";
+    
+    foreach ($dump['databases'] as $db=>$empty){
+
+        if ($dropDatabase){
+            $dropDb="DROP DATABASE `{$db}`;\n";
+            $dbExists="";
+        }
+        $createDatabases[$db]="{$dropDb}CREATE DATABASE {$dbExists} `{$db}`;";
+    }
+    foreach ($dump['tables'] as $db=>$table){
+        foreach($table as $tableName=>$tableCreationCode){
+            $createTables[$tableName]=$tableCreationCode;
+        }    
+    }
+    
+    $dump=array(
+        'databases'=>$createDatabases,
+        'tables'=>$createTables
+    );
+
+    return $dump;
+    
+}    
+/*
+ * database structure dump functions
+ * 
+ */
+ 
+function DatabasesAndTables($mysqli, $metadata){
+    
+    $databases=array();
+    $tables=array();
+    foreach ($metadata as $fc=>$meta){
+        $db=$meta->db;
+        $table=$meta->orgtable;
+        if (!array_key_exists($db, $databases)){
+            $databases[$db]='';
+        }
+        $key=$db.'.'.$table;
+        if (!array_key_exists($key, $tables)){
+            $result=$mysqli->query("SHOW CREATE TABLE `{$db}`.`{$table}`;");
+            
+            $row=$result->fetch_array(MYSQLI_NUM);
+            $tableName=array_keys($row);
+            //var_dump($row);
+            //exit();
+            $tables[$db][$table]=$row[1];
+            
+        }
+        return array('databases'=>$databases, 'tables'=>$tables);
+    }
+}
+
+
+
+?>
